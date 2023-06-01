91417569ffbd16e69fa3a2a1caba3473d6e4eab7
ahmedMetwally <ahmedmetwally577@gmail.com>
2023-06-01 03:11:18 +0300

    new version

---
+++ finder-app/finder.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# check if number of arguments = 2 or not.
+if [ ! $# -eq 2 ]; then
+	echo "ERROR: Invalid Number of Arguments."
+	echo "Total number of arguments should be 2."
+	echo "the order of the arguments should be:"
+	echo "	1) Path to the file."
+	echo "	2) String to be written in the specified file"
+	exit 1
+else
+	# check if the director is a real directory in my file system
+	if [ ! -d "$1" ]; then
+		echo "ERROR: The given Directory $1 doesn't exist!"
+		exit 1
+	fi
+fi
+
+# getting number of directories.
+#num_directories=`tree $1 | tail -1`
+num_directories=${num_directories#*, }
+
+# another way to get the number of files inside the directory and all subdirectories is:
+num_files=$(find $1 -type f | wc -l)
+# getting the number of matches line in the respective files
+searchStr=$2
+directory=$1
+num_lines=$(grep -r "$searchStr" "$directory" | wc -l)
+
+#echo "The number of files are $num_files"
+echo "The number of files are $num_files and the number of matching lines are $num_lines"
---
+++ finder-app/writer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# check if number of arguments = 2 or not.
+if [ ! $# -eq 2 ]; then
+	echo "ERROR: Invalid Number of Arguments."
+	echo "Total number of arguments should be 2."
+	echo "the order of the arguments should be:"
+	echo "	1) Path to the file."
+	echo "	2) String to be written in the specified file"
+	exit 1
+fi
+file_path=$1
+
+# Check if it possible to create a file to the path in argument 1
+if ! echo "$2" >"$file_path"; then
+	echo "Error: Could not create file at $file_path"
+	exit 1
+fi
